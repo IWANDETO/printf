63f1afe145988979142c93d466418851ca0de838
IWANDETO <wandettoian@gmail.com>
2022-10-23 15:29:52 -0700

    checked betty errors

--- printf.c
+++ printf.c
@@ -11,6 +11,9 @@ int _printf(const char *format, ...)
 {
 	va_list arg;
 	int i, j;
+	int count;
+	int total_count = 0;
+
 	print form_func[] = {
 		{"c", printf_char},
 		{"s", printf_string}
@@ -20,26 +23,29 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-			while (format[i] != '%')
-			{
-					_putchar(format[i]);
-						i++;
-			}
-		while (format && (*(format + i)))
+	while (format[i] != '%')
+	{
+		_putchar(format[i]);
+		i++;
+	}
+		for (j = 0; print[j].symbol != NULL; j++)
 		{
-		j = 0;
-
-			while (j < 4 && (*(format + i) != *(form_func[j].symbol)))
-			j++;
-
-			if (j < 4)
+			if (format[i + 1] == print[j].symbol[0])
 			{
-			form_func[j].prints(arg);
-			}	
-			i++;
+				count = print[j].prints(va_list arg);
+				if (count == -1)
+					return (-1);
+				total_count += count;
+				break;
+			}
+	}
+		if (print[j].symbol == NULL && format[i + 1] != ' ')
+		{
+			return (-1);
 		}
 	}
+
 	va_end(arg);
-	return (0);
+	return (total_count);
 }
 
